0d84f17b0009dc96e0b824ac8d5819838f885088
Takahiro Sugiura <takahiro.sugiura.0227@gmail.com>
2018-12-27 16:49:12 +0900

    Support spaces in inputs

--- 1cc.c
+++ 1cc.c
@@ -1,5 +1,64 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// values representing the types of tokens
+enum {
+  TK_NUM = 256,  // integer token
+  TK_EOF,  // end of file token
+};
+
+// type of token
+typedef struct {
+  int ty;       // type of token
+  int val;      // number if ty is TK_NUM
+  char *input;  // token string (for error message)
+} Token;
+
+// Tokenized tokens are preserved in this array.
+// It's assumed that the number of input tokens is less than 100.
+Token tokens[100];
+
+// divide a string pointed by p into tokens and preserve them in tokens
+void tokenize(char *p) {
+  int i = 0;
+  while (*p) {
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-') {
+      tokens[i].ty = *p;
+      tokens[i].input = p;
+      i++;
+      p++;
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      tokens[i].ty = TK_NUM;
+      tokens[i].input = p;
+      tokens[i].val = strtol(p, &p, 10);
+      i++;
+      continue;
+    }
+
+    fprintf(stderr, "トークナイズできません: %s\n", p);
+    exit(1);
+  }
+
+  tokens[i].ty = TK_EOF;
+  tokens[i].input = p;
+}
+
+// function to report error
+void error(int i) {
+  fprintf(stderr, "予期せぬトークンです: %s\n",
+          tokens[i].input);
+  exit(1);
+}
 
 int main(int argc, char const *argv[])
 {
@@ -7,31 +66,44 @@ int main(int argc, char const *argv[])
     fprintf(stderr, "引数の個数が正しくありません\n");
     return 1;
   }
-  char *p = argv[1];
+
+  tokenize(argv[1]);
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %ld\n", strtol(p, &p, 10));
 
-  while (*p) {
-    if (*p == '+') {
-      p++;
-      printf("  add rax, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
-
-    
-    if (*p == '-') {
-      p++;
-      printf("  sub rax, %ld\n", strtol(p, &p, 10));
-      continue;
-    }
-    
-    fprintf(stderr, "予期せぬ文字です: '%c'\n", *p);
-    return 1;
+  // The first token must be number.
+  if (tokens[0].ty != TK_NUM) {
+    error(0);
   }
-  
+  printf("  mov rax, %d\n", tokens[0].val);
+
+  int i = 1;
+  while (tokens[i].ty != TK_EOF) {
+    if (tokens[i].ty == '+') {
+      i++;
+      if (tokens[i].ty != TK_NUM) {
+        error(i);
+      }
+      printf("  add rax, %ld\n", tokens[i].val);
+      i++;
+      continue;
+    }
+    
+    if (tokens[i].ty == '-') {
+      i++;
+      if (tokens[i].ty != TK_NUM) {
+        error(i);
+      }
+      printf("  sub rax, %ld\n", tokens[i].val);
+      i++;
+      continue;
+    }
+
+    error(i);    
+  }
+
   printf("  ret\n");
   return 0;
 }
