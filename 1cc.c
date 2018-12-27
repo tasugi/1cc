307c9f44e2026f209f37311873886a9f6151864a
Takahiro Sugiura <takahiro.sugiura.0227@gmail.com>
2018-12-27 16:06:15 +0900

    Set the indent width to 2 spaces.

--- 1cc.c
+++ 1cc.c
@@ -3,15 +3,15 @@
 
 int main(int argc, char const *argv[])
 {
-    if (argc != 2) {
-        fprintf(stderr, "引数の個数が正しくありません\n");
-        return 1;
-    }
+  if (argc != 2) {
+    fprintf(stderr, "引数の個数が正しくありません\n");
+    return 1;
+  }
 
-    printf(".intel_syntax noprefix\n");
-    printf(".global main\n");
-    printf("main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));
-    printf("  ret\n");
-    return 0;
+  printf(".intel_syntax noprefix\n");
+  printf(".global main\n");
+  printf("main:\n");
+  printf("  mov rax, %d\n", atoi(argv[1]));
+  printf("  ret\n");
+  return 0;
 }
