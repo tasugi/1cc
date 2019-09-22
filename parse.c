721dac59d67474af3588c379d43bc1a503ce8975
Takahiro Sugiura <takahiro.sugiura.0227@gmail.com>
2019-09-22 18:57:22 +0900

    Refactor

--- parse.c
+++ parse.c
@@ -77,30 +77,6 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    if (strncmp(p, "while", 5) == 0 && !is_alnum(p[5])) {
-      cur = new_token(TK_WHILE, cur, p, 5);
-      p += 5;
-      continue;
-    }
-
-    if (strncmp(p, "else", 4) == 0 && !is_alnum(p[4])) {
-      cur = new_token(TK_ELSE, cur, p, 4);
-      p += 4;
-      continue;
-    }
-
-    if (strncmp(p, "if", 2) == 0 && !is_alnum(p[2])) {
-      cur = new_token(TK_IF, cur, p, 2);
-      p += 2;
-      continue;
-    }
-
-    if (strncmp(p, "return", 6) == 0 && !is_alnum(p[6])) {
-      cur = new_token(TK_RETURN, cur, p, 6);
-      p += 6;
-      continue;
-    }
-
     if (!strncmp(p, "==", 2) || !strncmp(p, "!=", 2) || !strncmp(p, "<=", 2) ||
         !strncmp(p, ">=", 2)) {
       cur = new_token(TK_RESERVED, cur, p, 2);
@@ -125,7 +101,17 @@ Token *tokenize(char *p) {
       int len = 1;
       while ('a' <= p[len] && p[len] <= 'z')
         len++;
-      cur = new_token(TK_IDENT, cur, p, len);
+      if (len == 6 && strncmp(p, "return", len) == 0) {
+        cur = new_token(TK_RETURN, cur, p, len);
+      } else if (len == 2 && strncmp(p, "if", len) == 0) {
+        cur = new_token(TK_IF, cur, p, len);
+      } else if (len == 4 && strncmp(p, "else", len) == 0) {
+        cur = new_token(TK_ELSE, cur, p, len);
+      } else if (len == 5 && strncmp(p, "while", len) == 0) {
+        cur = new_token(TK_WHILE, cur, p, len);
+      } else {
+        cur = new_token(TK_IDENT, cur, p, len);
+      }
       p += len;
       continue;
     }
