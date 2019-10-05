ccd73ac89ec29028a7f2dd8d1a894350367549fb
Takahiro Sugiura <takahiro.sugiura.0227@gmail.com>
2019-10-05 15:14:49 +0900

    Refactor

--- codegen.c
+++ codegen.c
@@ -17,19 +17,78 @@ void gen_lval(Node *node) {
   printf("  push rax\n");
 }
 
+void gen_be_prologue(Node *node) {
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+}
+
+// gen binary expression
+void gen_be(Node *node) {
+  switch (node->kind) {
+  case ND_ADD:
+    gen_be_prologue(node);
+    printf("  add rax, rdi\n");
+    break;
+  case ND_SUB:
+    gen_be_prologue(node);
+    printf("  sub rax, rdi\n");
+    break;
+  case ND_MUL:
+    gen_be_prologue(node);
+    printf("  imul rdi\n");
+    break;
+  case ND_DIV:
+    gen_be_prologue(node);
+    printf("  cqo\n");
+    printf("  idiv rdi\n");
+    break;
+  case ND_EQ:
+    gen_be_prologue(node);
+    printf("  cmp rax, rdi\n");
+    printf("  sete al\n");
+    printf("  movzb rax, al\n");
+    break;
+  case ND_NE:
+    gen_be_prologue(node);
+    printf("  cmp rax, rdi\n");
+    printf("  setne al\n");
+    printf("  movzb rax, al\n");
+    break;
+  case ND_LT:
+    gen_be_prologue(node);
+    printf("  cmp rax, rdi\n");
+    printf("  setl al\n");
+    printf("  movzb rax, al\n");
+    break;
+  case ND_LE:
+    gen_be_prologue(node);
+    printf("  cmp rax, rdi\n");
+    printf("  setle al\n");
+    printf("  movzb rax, al\n");
+    break;
+  default:
+    error("Unsupported node: %s", node->kind);
+  }
+
+  printf("  push rax\n");
+}
+
 void gen(Node *node) {
   int i = 0;
   int l_label;
   switch (node->kind) {
   case ND_NUM:
     printf("  push %d\n", node->val);
-    return;
+    break;
   case ND_LVAR:
     gen_lval(node);
     printf("  pop rax\n");
     printf("  mov rax, [rax]\n");
     printf("  push rax\n");
-    return;
+    break;
   case ND_ASSIGN:
     gen_lval(node->lhs);
     gen(node->rhs);
@@ -38,14 +97,14 @@ void gen(Node *node) {
     printf("  pop rax\n");
     printf("  mov [rax], rdi\n");
     printf("  push rdi\n");
-    return;
+    break;
   case ND_RETURN:
     gen(node->lhs);
     printf("  pop rax\n");
     printf("  mov rsp, rbp\n");
     printf("  pop rbp\n");
     printf("  ret\n");
-    return;
+    break;
   case ND_IF:
     gen(node->cond);
     printf("  pop rax\n");
@@ -55,7 +114,7 @@ void gen(Node *node) {
       printf("  je .Lend%d\n", l_label);
       gen(node->then);
       printf(".Lend%d:\n", l_label);
-      return;
+      break;
     }
     printf("  je .Lelse%d\n", l_label);
     gen(node->then);
@@ -63,7 +122,7 @@ void gen(Node *node) {
     printf(".Lelse%d:\n", l_label);
     gen(node->els);
     printf(".Lend%d:\n", l_label);
-    return;
+    break;
   case ND_WHILE:
     l_label = label++;
     printf(".Lbegin%d:\n", l_label);
@@ -74,7 +133,7 @@ void gen(Node *node) {
     gen(node->body);
     printf("  jmp .Lbegin%d\n", l_label);
     printf(".Lend%d:\n", l_label);
-    return;
+    break;
   case ND_FOR:
     l_label = label++;
     gen(node->init);
@@ -87,60 +146,14 @@ void gen(Node *node) {
     gen(node->inc);
     printf("  jmp .Lbegin%d\n", l_label);
     printf(".Lend%d:\n", l_label);
-    return;
+    break;
   case ND_BLOCK:
     while (node->stmts[i] != NULL) {
       gen(node->stmts[i++]);
       printf("  pop rax\n");
     }
-    return;
-  }
-
-  gen(node->lhs);
-  gen(node->rhs);
-
-  printf("  pop rdi\n");
-  printf("  pop rax\n");
-
-  switch (node->kind) {
-  case ND_NUM: // suppress warning
-    break;
-  case ND_ADD:
-    printf("  add rax, rdi\n");
-    break;
-  case ND_SUB:
-    printf("  sub rax, rdi\n");
-    break;
-  case ND_MUL:
-    printf("  imul rdi\n");
-    break;
-  case ND_DIV:
-    printf("  cqo\n");
-    printf("  idiv rdi\n");
-    break;
-  case ND_EQ:
-    printf("  cmp rax, rdi\n");
-    printf("  sete al\n");
-    printf("  movzb rax, al\n");
-    break;
-  case ND_NE:
-    printf("  cmp rax, rdi\n");
-    printf("  setne al\n");
-    printf("  movzb rax, al\n");
-    break;
-  case ND_LT:
-    printf("  cmp rax, rdi\n");
-    printf("  setl al\n");
-    printf("  movzb rax, al\n");
-    break;
-  case ND_LE:
-    printf("  cmp rax, rdi\n");
-    printf("  setle al\n");
-    printf("  movzb rax, al\n");
     break;
   default:
-    error("Unsupported node: %s", node->kind);
+    gen_be(node);
   }
-
-  printf("  push rax\n");
 }
